bf6caa6589a8d5327ddc9f5c4854e50ca14775a9
Serdar Durdyev <durdyev.s@nakukop.com>
2023-09-20 21:56:13 +0300

    Add file upload max size

--- src/Form/NewGoodType.php
+++ src/Form/NewGoodType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -43,7 +43,13 @@ class NewGoodType extends AbstractType
                 ),
                 'constraints' => [
                     new NotBlank(),
-                    new Image(['maxSize' => '10m'])
+                    new File([
+                        'maxSize' => '20M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
                 ]
             ])
             ->add(
--- src/Form/UpdateGoodType.php
+++ src/Form/UpdateGoodType.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -41,7 +41,13 @@ class UpdateGoodType extends AbstractType
                     'accept' => 'image/*',
                 ),
                 'constraints' => [
-                    new Image(['maxSize' => '10m'])
+                    new File([
+                        'maxSize' => '20M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
                 ]
             ])
             ->add(
